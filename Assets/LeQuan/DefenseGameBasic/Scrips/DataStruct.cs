3f8512ef8e693924c91e8c1acad81f463be96031
quanptit1996@gmail.com <quanptit1996@gmail.com>
2022-07-15 18:18:55 +0700

    ShopManager

---
+++ Assets/LeQuan/DefenseGameBasic/Scrips/DataStruct.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LQ.DefenseBasic
+{
+    [Serializable]
+    public class ShopItem
+    {
+        public PlayerController playerPrefab;
+        public int price;
+        public Sprite previewImg;
+    }
+}
+
---
+++ Assets/LeQuan/DefenseGameBasic/Scrips/ShopManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using LQ.DefenseBasic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    [SerializeField] private ShopItem[] items;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        if (items != null )
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                var item = items[i];
+                string dataKey = Const.PLAYER_PREFIX_PREF+i; //player_0   player_1  player_2
+                if (item != null)
+                {
+                    if (i == 0)
+                    {
+                        Pref.SetBool(dataKey,true);
+                    }
+                    else
+                    {
+                        if (!PlayerPrefs.HasKey(dataKey))
+                        {
+                            Pref.SetBool(dataKey,false);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
